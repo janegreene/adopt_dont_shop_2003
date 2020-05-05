bf758f2b2b743043cb34cf1e1e6f3858b0a3450e
Jane Greene <janegreenecasa@gmail.com>
2020-05-05 16:49:03 -0600

    completed user story 3

---
+++ spec/features/shelters/show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+
+RSpec.describe "shelter show page", type: :feature do
+  it "can see the shelter details" do
+    shelter_1 = Shelter.create(name: "Pawistive Tomorrows",
+                         address:      "123 W 66th Ave.",
+                         city:  "Denver",
+                         state:      "CO",
+                         zip:  80221)
+    shelter_2 = Shelter.create(name: "Denver Animal Shelter",
+                         address:       "4123 W 86th Pl.",
+                         city:  "Fort Collins",
+                         state:      "CO",
+                         zip:  82126)
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+    expect(page).not_to have_content(shelter_2.name)
+    expect(page).not_to have_content(shelter_2.address)
+    expect(page).not_to have_content(shelter_2.city)
+    expect(page).not_to have_content(shelter_2.zip)
+  end
+end
