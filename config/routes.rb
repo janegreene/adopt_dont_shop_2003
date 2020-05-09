12e5bd18b3b454144582fa4511f9af5473399633
Jane Greene <janegreenecasa@gmail.com>
2020-05-09 17:12:15 -0600

    completed user story 14

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get '/shelters/:id/edit', to: 'shelters#edit'
     patch '/shelters/:id', to: 'shelters#update'
     delete '/shelters/:id', to: 'shelters#delete'
+    get '/shelters/:id/delete', to: 'shelters#delete' 
     get '/pets', to: 'pets#index'
     get '/pets/:id', to: 'pets#show'
     get '/pets/:id/edit', to: 'pets#edit'
--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -37,7 +37,35 @@ RSpec.describe "shelter index page", type: :feature do
     expect(current_path).to eq("/shelters/#{shelter_2.id}/edit")
 
   end
+  it "next to every shelter is a link to delete that shelter" do
+    shelter_1 = Shelter.create(name: "Pawistive Tomorrows",
+                         address:      "123 W 66th Ave.",
+                         city:  "Denver",
+                         state:      "CO",
+                         zip:  80221)
+    shelter_2 = Shelter.create(name: "Denver Animal Shelter",
+                         address:       "4123 W 86th Pl.",
+                         city:  "Denver",
+                         state:      "CO",
+                         zip:  82126)
+
+    visit "/shelters"
+    click_link "Delete #{shelter_2.name}"
+
+    expect(current_path).to eq("/shelters")
+    expect(page).to have_content(shelter_1.name)
+    expect(page).not_to have_content(shelter_2.name)
+
+  end
 end
+# User Story 14, Shelter Delete From Shelter Index Page
+#
+# As a visitor
+# When I visit the shelter index page
+# Next to every shelter, I see a link to delete that shelter
+# When I click the link
+# I am returned to the Shelter Index Page where I no longer see that shelter
+
 # User Story 13, Shelter Update From Shelter Index Page
 #
 # As a visitor
