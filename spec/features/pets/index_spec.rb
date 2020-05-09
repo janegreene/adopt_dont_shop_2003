464ce21a50fe952629df51765285df013add5e7f
Jane Greene <janegreenecasa@gmail.com>
2020-05-09 17:34:59 -0600

    completed user story 16

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -50,7 +50,36 @@ RSpec.describe "pets index page", type: :feature do
     click_link "Edit #{pet_2.name}"
 
     expect(current_path).to eq("/pets/#{pet_2.id}/edit")
-    
+
+  end
+
+  it "next to every pet is a link to delete that pet" do
+    shelter_1 = Shelter.create(name: "Pawistive Tomorrows",
+                         address:      "123 W 66th Ave.",
+                         city:  "Denver",
+                         state:      "CO",
+                         zip:  80221)
+     pet_1 = Pet.create(image: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                        name:      "Randy",
+                        description: "Lots of fun",
+                        approximate_age:  "3 years",
+                        sex:      "male",
+                        shelter_id: shelter_1.id)
+     pet_2 = Pet.create(image: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                        name:      "Lucy",
+                        description: "Great sense of design",
+                        approximate_age:  "6 months",
+                        sex:      "female",
+                        shelter_id: shelter_1.id)
+
+
+    visit "/pets"
+    click_link "Delete #{pet_2.name}"
+
+    expect(current_path).to eq("/pets")
+    expect(page).to have_content(pet_1.name)
+    expect(page).not_to have_content(pet_2.name)
+
   end
 end
 # User Story 15, Pet Update From Pets Index Page
