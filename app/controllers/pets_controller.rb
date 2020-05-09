374eb91753471aa5bf2e81a7f6ecb6cccc325673
Jane Greene <janegreenecasa@gmail.com>
2020-05-09 15:30:46 -0600

    completed story 11 update pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,5 +7,20 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
 
+  def edit
+    @pet_id = params[:id]
+  end
 
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update(pet_params)
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  private
+
+  def pet_params
+    params.permit(:image, :name, :description, :approximate_age, :sex, :adoption_status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     delete '/shelters/:id', to: 'shelters#delete'
     get '/pets', to: 'pets#index'
     get '/pets/:id', to: 'pets#show'
+    get '/pets/:id/edit', to: 'pets#edit'
+    post 'pets/:id/edit', to: 'pets#update' 
     get '/shelters/:id/pets', to: 'shelter_pets#index'
     get '/shelters/:id/pets/new', to: 'shelter_pets#new'
     post '/shelters/:id/pets', to: 'shelter_pets#create'
