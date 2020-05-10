1cd7c7131639b88ac93c3ed9d4c631cf31a1a0a4
Jane Greene <janegreenecasa@gmail.com>
2020-05-10 16:39:40 -0600

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,11 @@ shelter_1 = Shelter.create(name: "Pawsitive Tomorrows",
                      city:  "Denver",
                      state:      "CO",
                      zip:  80221)
+shelter_2 = Shelter.create(name: "Pet Palace",
+                    address:      "334 West Ave.",
+                    city:  "Thornton",
+                    state:      "CO",
+                    zip:  98998)
 pet_1 = Pet.create(image: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
                    name:      "Randy",
                    approximate_age:  "3 years",
@@ -19,10 +24,36 @@ pet_2 = Pet.create(image: "https://images.unsplash.com/photo-1518020382113-a7e8f
                    name:      "Lucy",
                    approximate_age:  "6 months",
                    sex:      "female",
-                   shelter_id: shelter_1.id)
+                   shelter_id: shelter_1.id,
+                   description: "Lucy should not be trusted around fire.",
+                   adoption_status: "adoptable")
 
 pet_3 = Pet.create(image: "https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
                    name:      "Larry",
                    approximate_age:  "9 months",
                    sex:      "male",
-                   shelter_id: shelter_1.id)
+                   shelter_id: shelter_1.id,
+                   description: "Larry will eat your homework.",
+                   adoption_status: "adoptable")
+
+ pet_4 = Pet.create(image: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                    name:      "Randy",
+                    approximate_age:  "3 years",
+                    sex:      "male",
+                    shelter_id: shelter_2.id,
+                    description: "Randy is a playful pup who likes short walks on long beaches.",
+                   adoption_status: "adoptable")
+ pet_5 = Pet.create(image: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                    name:      "Lucy",
+                    approximate_age:  "6 months",
+                    sex:      "female",
+                    shelter_id: shelter_2.id,
+                    description: "Lucy loves bathtime and reading romance novels aloud.",
+                    adoption_status: "pending")
+ pet_6 = Pet.create(image: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                   name:      "Olive",
+                   approximate_age:  "10 months",
+                   sex:      "female",
+                   shelter_id: shelter_2.id,
+                   description: "Olive is going through a punk rock phase and exculsively wears vegan leather.",
+                   adoption_status: "adoptable")
