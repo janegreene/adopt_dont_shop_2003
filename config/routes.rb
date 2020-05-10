5d6222966e85e86eee518aa15ebcc0b18b8380a2
Jane Greene <janegreenecasa@gmail.com>
2020-05-10 14:48:33 -0600

    deleted unnecissary welcome route and wip css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-    root 'welcome#index'
     get '/shelters', to: 'shelters#index'
     get '/shelters/new', to: 'shelters#new'
     get '/shelters/:id', to: 'shelters#show'
